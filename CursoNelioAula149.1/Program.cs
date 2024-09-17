0085e7cd58f110af39ba9c3a99ac3a851866d02f
ivanostorari <programadordev41@gmail.com>
2024-09-17 16:52:57 -0400

    CursoNelioAula149.1 e 149.2

---
+++ CursoNelioAula149.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using CursoNelioAula14901;
+
+namespace CursoNelioAula1491
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i=0; i< n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            int a = (int)printService.Fist();
+            int b = a + 2;
+            Console.WriteLine(b);
+
+            printService.Print();
+            Console.WriteLine("Fist: " + printService.Fist());
+        }
+    }
+}
---
+++ CursoNelioAula149.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using CursoNelioAula14902;
+
+namespace CursoNelioAula1491
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+            
+            printService.Print();
+            Console.WriteLine("Fist: " + printService.Fist());
+        }
+    }
+}
