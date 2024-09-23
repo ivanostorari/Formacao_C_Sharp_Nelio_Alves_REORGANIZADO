abbb4997733449180ef4211c826beb98f6c5a7eb
ivanostorari <programadordev41@gmail.com>
2024-09-23 15:56:46 -0400

    CursoNelioAula151.0

---
+++ CursoNelioAula151.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CursoNelioAula1510
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Maria";
+            string b = "Alex";
+
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
