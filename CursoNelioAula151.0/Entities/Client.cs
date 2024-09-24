26910cc2b5ab171cd4aa2a77a6bf3bc511ca21f9
ivanostorari <programadordev41@gmail.com>
2024-09-24 16:30:22 -0400

    CursoNelioAula151.0

---
+++ CursoNelioAula151.0/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CursoNelioAula1510.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
--- CursoNelioAula151.0/obj/Debug/net6.0/CursoNelioAula151.0.AssemblyInfo.cs
+++ CursoNelioAula151.0/obj/Debug/net6.0/CursoNelioAula151.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoNelioAula151.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abbb4997733449180ef4211c826beb98f6c5a7eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+683d62f01424635d1662359756840394bc04060a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoNelioAula151.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoNelioAula151.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
