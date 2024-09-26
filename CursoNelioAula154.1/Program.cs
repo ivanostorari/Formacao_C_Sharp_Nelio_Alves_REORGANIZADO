d14dfbdd336cb2536e35618a4289ee51fd5bed4d
ivanostorari <programadordev41@gmail.com>
2024-09-26 15:09:05 -0400

    CursoNelioAula154.1

---
+++ CursoNelioAula154.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CursoNelioAula1541.Entities;
+
+namespace CursoNelioAula1541
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("Notebook", 1200.00));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.00);
+
+            Console.WriteLine(a.Contains(prod));
+
+
+        }
+    }
+}
