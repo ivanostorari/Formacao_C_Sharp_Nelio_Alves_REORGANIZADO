d5bd72dce880fcf3d88a626365ac709f2882b411
ivanostorari <programadordev41@gmail.com>
2024-09-26 16:45:36 -0400

    CursoNelioAula155.0

---
+++ CursoNelioAula155.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CursoNelioAula155 0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
