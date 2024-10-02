da49203b36d0f9709255a557bba7e412209eba0c
ivanostorari <programadordev41@gmail.com>
2024-10-02 16:01:22 -0400

    CursoNelioAula161.1

---
+++ CursoNelioAula161.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CursoNelioAula1611.Extensions;
+
+public class ProcessFile
+{
+    public static void Main()
+    {
+        DateTime dt = new DateTime(2018, 12, 16, 8, 10, 45);
+        Console.WriteLine(dt.ElapsedTime());
+
+
+    }
+}
