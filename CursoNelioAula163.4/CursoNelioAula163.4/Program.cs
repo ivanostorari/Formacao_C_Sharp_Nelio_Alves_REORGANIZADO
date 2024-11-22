8bccc2395efbde1c2e4026418fb3c38e6aa95828
ivanostorari <programadordev41@gmail.com>
2024-11-22 11:33:09 -0400

    CursoNelioAula163.4

---
+++ CursoNelioAula163.4/CursoNelioAula163.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CursoNelioAula1634.Entities;
+
+namespace CursoNelioAula1634
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            list.Sort(comp);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
