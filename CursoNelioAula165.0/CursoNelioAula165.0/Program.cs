de3da4848c27eafdbb2b4c754ac3ff6195864e79
ivanostorari <programadordev41@gmail.com>
2024-11-25 08:29:08 -0400

    CursoNelioAula165.0

---
+++ CursoNelioAula165.0/CursoNelioAula165.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using CursoNelioAula1650.Services;
+using System;
+
+namespace CursoNelioAula1650
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            double result = CalculationService.Sum(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
