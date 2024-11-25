7a1ba04e561e3d7a2a096aedd28cf057709e0271
ivanostorari <programadordev41@gmail.com>
2024-11-25 10:44:18 -0400

    CursoNelioAula166.0

---
+++ CursoNelioAula166.0/CursoNelioAula166.0/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using CursoNelioAula1660.Services;
+using System;
+
+namespace CursoNelioAula1660
+{
+
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+            //posso também alternativamente colocar só op(a, b); que funciona da mesma forma.
+        }
+    }
+}
