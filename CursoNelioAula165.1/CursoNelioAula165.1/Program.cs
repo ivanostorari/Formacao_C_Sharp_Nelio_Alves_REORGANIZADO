f100a59ec485851817d95af38704d9de3c3ffd08
ivanostorari <programadordev41@gmail.com>
2024-11-25 09:03:32 -0400

    CursoNelioAula165.1

---
+++ CursoNelioAula165.1/CursoNelioAula165.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using CursoNelioAula1651.Services;
+using System;
+
+namespace CursoNelioAula1651
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            double result = CalculationService.Max(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
