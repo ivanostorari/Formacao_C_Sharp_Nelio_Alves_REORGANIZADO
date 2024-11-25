49bbc4a5561ad6c8709707e6658b43da29b84ba4
ivanostorari <programadordev41@gmail.com>
2024-11-25 09:57:19 -0400

    CursoNelioAula165.4

---
+++ CursoNelioAula165.4/CursoNelioAula165.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using CursoNelioAula1654.Services;
+using System;
+
+namespace CursoNelioAula1654
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Max;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
--- CursoNelioAula165.4/CursoNelioAula165.4/obj/Debug/net6.0/CursoNelioAula165.4.AssemblyInfo.cs
+++ CursoNelioAula165.4/CursoNelioAula165.4/obj/Debug/net6.0/CursoNelioAula165.4.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CursoNelioAula165.3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CursoNelioAula165.4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675afb4de06befceb78dffb647c671877dba207d")]
-[assembly: System.Reflection.AssemblyProductAttribute("CursoNelioAula165.3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CursoNelioAula165.3")]
+[assembly: System.Reflection.AssemblyProductAttribute("CursoNelioAula165.4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CursoNelioAula165.4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
