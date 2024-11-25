52f9df0928b48cfd9108ffb9ac5df5bcd82fe07d
ivanostorari <programadordev41@gmail.com>
2024-11-25 10:26:03 -0400

    CursoNelioAula165.6

---
+++ CursoNelioAula165.6/CursoNelioAula165.6/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using CursoNelioAula1656.Services;
+using System;
+
+namespace CursoNelioAula1656
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            // fica muito verboso, então muito mais prático usar a função (CalculationService.Sum);
+
+            double result = op.Invoke(a, b);
+            // pode usar outra função chamada Invoke é outra sintaxe alternativa.
+            Console.WriteLine(result);
+        }
+    }
+}
