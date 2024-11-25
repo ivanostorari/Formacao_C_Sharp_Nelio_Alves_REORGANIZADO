675afb4de06befceb78dffb647c671877dba207d
ivanostorari <programadordev41@gmail.com>
2024-11-25 09:31:20 -0400

    CursoNelioAula165.3

---
+++ CursoNelioAula165.3/CursoNelioAula165.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using CursoNelioAula1653.Services;
+using System;
+
+namespace CursoNelioAula1653
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
