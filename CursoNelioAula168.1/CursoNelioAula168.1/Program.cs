cd537e082e91bee01d63fc37fe9c3cead701ad01
ivanostorari <programadordev41@gmail.com>
2024-11-26 09:27:32 -0400

    CursoNelioAula168.1

---
+++ CursoNelioAula168.1/CursoNelioAula168.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using CursoNelioAula1681.Entities;
+
+namespace CursoNelioAula1681
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(UpdatePrice);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
