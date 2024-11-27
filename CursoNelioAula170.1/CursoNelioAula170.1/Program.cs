2674a9b456a437e6d7f945563a5d467ea24f9dd4
ivanostorari <programadordev41@gmail.com>
2024-11-27 10:52:52 -0400

    CursoNelioAula170.0

--- CursoNelioAula170.1/CursoNelioAula170.1/Program.cs
+++ CursoNelioAula170.1/CursoNelioAula170.1/Program.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 using System.Linq;
 
-namespace CursoNelioAula1700
+namespace CursoNelioAula1701
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Specify the data source/ -> Especificar a fonte dos dados, lugar de onde vão vir os dados.
+            //Specify the data source/ -> Especificar a fonte dos dados, lugar de onde vão vir os dados.
             int[] numbers = new int[] { 2, 3, 4, 5 };
 
-            // Define the query expression/ -> Definir a consulta dos dados, e escrever a expressão com Linq.
+            //Define the query expression/ -> Definir a consulta dos dados, e escrever a expressão com Linq.
             var result = numbers
                 .Where(x => x % 2 == 0)
                 .Select(x => x * 10);
 
             // Executar the query/ -> Executar a consulta dos dados, vai ser utilizando um foreach ou por outra operação terminal, é uma operação q vai forçar essa consulta ser executada.
-            foreach (int x in result) 
+            foreach (int x in result)
             {
                 Console.WriteLine(x);
-            }           
+            }
         }
     }
 }
