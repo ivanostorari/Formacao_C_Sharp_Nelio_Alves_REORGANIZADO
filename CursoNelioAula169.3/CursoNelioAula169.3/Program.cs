56f2888a37dd95809b02ea75dd6047a680bf2aea
ivanostorari <programadordev41@gmail.com>
2024-11-27 09:59:51 -0400

    CursoNelioAula169.3

---
+++ CursoNelioAula169.3/CursoNelioAula169.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CursoNelioAula1693.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CursoNelioAula1693
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();// metodo in line
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
