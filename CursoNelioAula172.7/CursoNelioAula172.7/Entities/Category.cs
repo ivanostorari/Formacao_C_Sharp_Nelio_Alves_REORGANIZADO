5df78b0f2978f5a7fa1b70ac597069feb43eee46
ivanostorari <programadordev41@gmail.com>
2024-11-29 10:29:32 -0400

    CursoNelioAula172.7

---
+++ CursoNelioAula172.7/CursoNelioAula172.7/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace CursoNelioAula1727.Entities
+{
+        class Category
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Tier { get; set; }
+        }
+}
